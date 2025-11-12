259719460516914a8f026b1051e9628ad0256fa9
이강우 <kangwoolee@igang-uui-Macmini.local>
2025-11-12 23:38:48 +0900

    CRUD

--- lib/data/app_database.dart
+++ lib/data/app_database.dart
@@ -1,4 +1,3 @@
-import 'dart:io';
 import 'package:drift/drift.dart';
 import 'package:drift_flutter/drift_flutter.dart';
 import 'package:path_provider/path_provider.dart';
@@ -18,6 +17,24 @@ class AppDatabase extends _$AppDatabase {
 
   @override
   int get schemaVersion => 1;
+
+  // -- CRUD helpers
+  Future<List<Todo>> getAll() => select(todos).get();
+  Stream<List<Todo>> watchAll() =>
+      (select(todos)..orderBy([(t) => OrderingTerm.desc(t.id)])).watch();
+
+  Future<int> insertTodo(String title) =>
+      into(todos).insert(TodosCompanion.insert(title: title));
+
+  Future<int> toggle(int id, bool next) => (update(
+    todos,
+  )..where((t) => t.id.equals(id))).write(TodosCompanion(done: Value(next)));
+
+  Future<int> remove(int id) =>
+      (delete(todos)..where((t) => t.id.equals(id))).go();
+
+  Future<int> clearDone() =>
+      (delete(todos)..where((t) => t.done.equals(true))).go();
 
   static _openConnection() {
     return driftDatabase(
